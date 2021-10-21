4503c37c9ed646cde0eb295405417b5c9eb0c5fa
Ronald Petty <ronald.petty@rx-m.com>
2021-10-21 23:52:52 +0000

    initial commit

---
+++ pkg1/code.go
@@ -0,0 +1,7 @@
+package pkg1
+
+import "fmt"
+
+func Runme() {
+	fmt.Println("module3/ronaldpetty/pkg1/code.Runme")
+}
---
+++ pkg2/code.go
@@ -0,0 +1,7 @@
+package pkg2
+
+import "fmt"
+
+func Runme() {
+	fmt.Println("module3/ronaldpetty/pkg2/code.Runme")
+}
